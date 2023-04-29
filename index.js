3e6e6799da6f1283a3e0e35182b2a08b4866113d
thwahatkp <thwahatkp@gmail.com>
2023-04-29 11:02:42 +0530

    added footer

---
+++ index.js
@@ -0,0 +1,16 @@
+let showPass = () => {
+  let passIcon = document.querySelector("#password-icon");
+  let pass = document.querySelector("#password");
+  if (pass.type === "password") {
+    pass.type = "text";
+    passIcon.innerHTML = "&#xf070;";
+  } else {
+    pass.type = "password";
+    passIcon.innerHTML = "&#xf06e;";
+  }
+};
+
+document.querySelector("#password-icon").addEventListener("click", showPass);
+document
+  .querySelector("#email")
+  .addEventListener("input", (e) => alert(e.target.value));
